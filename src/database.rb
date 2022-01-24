7b9099d0474961d17952fc3ef93e323c573c354c
Harry Lewis <harry.lewis@queensu.ca>
2022-01-23 21:58:50 -0500

    Add table proxy operations to Database

--- src/database.rb
+++ src/database.rb
@@ -8,4 +8,20 @@ class Database
   def create_table(name:, schema:)
     @tables[name] = Table.new(name: name, schema: schema)
   end
+
+  def insert(table:, rows:)
+    @tables[table].insert(rows: rows)
+  end
+
+  def select(table:, column:, value:)
+    @tables[table].where(column: column, value: value)
+  end
+
+  def create_index(table:, column:)
+    @tables[table].create_index(column: column)
+  end
+
+  def delete_index(table:, column:)
+    @tables[table].delete_index(column: column)
+  end
 end
